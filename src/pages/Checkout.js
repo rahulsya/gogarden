64622afbab797b9b539643d0ccf403b92e29c649
rahulsya <48597821+rahulsya@users.noreply.github.com>
2020-09-08 00:59:20 +0700

    show order list

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -2,86 +2,58 @@ import React from "react";
 import { FaArrowLeft } from "react-icons/fa";
 import { connect } from "react-redux";
 
+import CheckoutForm from "../components/form/CheckoutForm";
+
 function Checkout({ cart, total, amount }) {
   return (
     <div className="container mx-auto px-5">
-      <div className="capitalize mt-5 mb-5 text-md flex items-center">
+      <div className="capitalize mt-5 mb-5 text-md flex items-center text-gray-800">
         <FaArrowLeft /> Back to cart
       </div>
       <div className="grid grid-cols-3 gap-4">
         <div className="col-span-2">
-          <div className="title text-lg font-semibold">Complete Address</div>
-          {/* form here */}
-          <div className="form  ">
-            <div className="flex mt-5">
-              <div className="w-full px-3">
-                <label
-                  className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2"
-                  htmlFor="grid-last-name"
-                >
-                  Full Name
-                </label>
-                <input
-                  className="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4"
-                  id="grid-last-name"
-                  type="text"
-                  placeholder="Doe"
-                />
-              </div>
-            </div>
-            <div className=" md:flex mb-2">
-              <div className="md:w-1/2 px-3 mb-6 md:mb-0">
-                <label
-                  className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2"
-                  for="grid-city"
-                >
-                  City
-                </label>
-                <input
-                  className="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4"
-                  id="grid-city"
-                  type="text"
-                  placeholder="Albuquerque"
-                />
-              </div>
-              <div className="md:w-1/2 px-3">
-                <label
-                  className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2"
-                  for="grid-state"
-                >
-                  State
-                </label>
-                <div className="relative">
-                  <select
-                    className="block appearance-none w-full bg-grey-lighter border border-grey-lighter text-grey-darker py-3 px-4 pr-8 rounded"
-                    id="grid-state"
-                  >
-                    <option>New Mexico</option>
-                    <option>Missouri</option>
-                    <option>Texas</option>
-                  </select>
+          <CheckoutForm />
+        </div>
+        <div className="col-span-1 ">
+          <div className="font-semibold text-xl mb-5 mt-5 text-gray-800">
+            Order List
+          </div>
+          {cart.map((cart) => {
+            return (
+              <div
+                className="flex flex-row 
+          items-center
+          justify-between 
+          font-semibold
+          text-white
+          px-3 py-3 rounded shadow-xl
+          bg-green-600
+          mb-4"
+                key={cart.id}
+              >
+                <div>
+                  <img
+                    src={cart.images[0]}
+                    alt={`cart-${cart.images[0]}`}
+                    className="rounded-full w-12 h-12"
+                  />
+                </div>
+                <div>{cart.name}</div>
+                <div>
+                  {cart.quantity} x ${cart.price}
                 </div>
               </div>
-              <div className="md:w-1/2 px-3">
-                <label
-                  className="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2"
-                  for="grid-zip"
-                >
-                  Zip
-                </label>
-                <input
-                  className="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4"
-                  id="grid-zip"
-                  type="text"
-                  placeholder="90210"
-                />
-              </div>
-            </div>
+            );
+          })}
+
+          <div
+            className="font-semibold text-xl mb-5 mt-5 
+          text-right
+          text-gray-800
+          "
+          >
+            Total ${total}
           </div>
-        </div>
-        <div className="col-span-1 bg-gray-400">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum,
-          fugit!
         </div>
       </div>
     </div>
