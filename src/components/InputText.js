eb5e8c5ae2d72c2de8e2a0e4bd29df3dba69d5f7
rahulsya <48597821+rahulsya@users.noreply.github.com>
2020-09-11 19:33:33 +0700

    update input text and emptycart

--- src/components/InputText.js
+++ src/components/InputText.js
@@ -1,6 +1,7 @@
 import React from "react";
+import { string, func } from "prop-types";
 
-export default function InputText({ name, type, onChange }) {
+export default function InputText({ name, type, onChange, placeholder }) {
   return (
     <div>
       <input
@@ -8,7 +9,20 @@ export default function InputText({ name, type, onChange }) {
         className="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4"
         type={type}
         onChange={onChange}
+        placeholder={placeholder}
       />
     </div>
   );
 }
+
+InputText.defaultProps = {
+  type: "text",
+  placeholder: "please type here..",
+};
+
+InputText.propType = {
+  name: string.isRequired,
+  type: string.isRequired,
+  onChange: func.isRequired,
+  placeholder: string.isRequired,
+};
--- src/components/emptyCart.js
+++ src/components/emptyCart.js
@@ -2,13 +2,13 @@ import React from "react";
 import { ReactComponent as EmptySvg } from "../assets/images/empty.svg";
 import { Link } from "react-router-dom";
 
-export default function emptyCart() {
+export default function emptyCart({ message }) {
   return (
     <div className="container mx-auto px-5 mt-5">
       <div className="flex flex-col justify-center items-center">
         <EmptySvg className="mx-auto" />
         <div className="capitalize text-center font-semibold text-2xl mb-4 text-green-800">
-          your cart is empty
+          {message}
         </div>
         <Link
           to="/products"
